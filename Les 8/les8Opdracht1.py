34b91b8f779de2f156769466e15ee99726147557
Jolanda van Hoorn <j.vanhoorn@bkr.nl>
2024-11-20 13:14:08 +0100

    les8 oefenopdrachten

--- Les 8/les8Opdracht1.py
+++ Les 8/les8Opdracht1.py
@@ -1,10 +1,5 @@
-b = 2
-def mijn_functie():
-    global b
-    b = b + 10
-    print("binnen:", b)
-
-print("buiten: ", b)
-mijn_functie()
-print("buiten:",b)
-
+a = 10
+def maal_drie(a):
+    print(a)
+    return a * 3
+print (a)

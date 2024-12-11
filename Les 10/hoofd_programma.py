ed1d2589b52048ef2065debae1d6e28c15275ab0
Jolanda van Hoorn <j.vanhoorn@bkr.nl>
2024-12-11 15:07:20 +0100

    Huiswerk les 10

---
+++ Les 10/hoofd_programma.py
@@ -0,0 +1,4 @@
+from mijn_pakket import speelgoed, voeg_namen_samen
+
+print(speelgoed[0])
+print(voeg_namen_samen("Jan", "Janssen"))
---
+++ Les 10/mijn_pakket.py
@@ -0,0 +1,9 @@
+speelgoed = ["bal", "x-box", "puzzel"]
+
+def voeg_namen_samen(voornaam, achternaam):
+    return voornaam + " " + achternaam
+
+import mijn_pakket
+
+print(speelgoed[0])
+print(voeg_namen_samen("Jan", "Janssen"))
